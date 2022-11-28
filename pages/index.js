18830c8b799f73889730e13372d4f0eaa3f51ee1
Dipankaj <dipankajsingh2@gmail.com>
2022-11-28 23:35:36 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ import { useState } from 'react'
 
 export default function Home() {
   const [text, setText] = useState("")
+
+  const [loggedIn, setLoggedIn] = useState(false)
   const id = 'Dipankaj'
+
   const saveToJson = async () => {
     const res = await fetch('http://localhost:3000/api/storedata', {
       method: 'POST',
@@ -16,28 +19,40 @@ export default function Home() {
     const data = await res.json()
     console.log(data)
   }
+
   return (<>
     <nav className={styles.nav}>
       <p>
         <span>TaxT</span>
         <span>Share</span>
       </p>
-      <button>LogOut</button>
-    </nav>
-    <form method='POST' onSubmit={(e) => e.preventDefault()} className={styles.form}>
       <div>
-        <input
-          type="text"
-          name='text'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder='Enter text to share!'
-        />
-        <button type='submit' onClick={saveToJson}>
-          <Send />
-        </button>
+        <div className={styles.Id}>Dipankaj</div>
+        <button>LogOut</button>
       </div>
-    </form>
+    </nav>
+    <div className={styles.container}>
+      <form method='POST' onSubmit={(e) => e.preventDefault()} className={styles.form}>
+        <div>
+          <label className={styles.textLabel} htmlFor="text">{"Please Enter An Old Or New Id Below!"}</label>
+          <input
+            type="text"
+            id='text'
+            name='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder={"Ex: Dipankaj_123singh"}
+          />
+          <button type='submit' onClick={saveToJson}>
+            <Send />
+          </button>
+        </div>
+      </form>
+      <div className={styles.prompts}>
+        <p className={styles.promptsText}>success</p>
+        <span className={styles.promptsIcon}>@</span>
+      </div>
+    </div>
   </>
   )
 }
